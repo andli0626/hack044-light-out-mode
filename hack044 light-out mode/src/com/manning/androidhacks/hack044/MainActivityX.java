e62fbaa97e0b29ede62f929b6b2b33642597f5b8
李林 <ww1095@163.com>
2016-06-03 16:30:36 +0800

    init

---
+++ hack044 light-out mode/src/com/manning/androidhacks/hack044/MainActivityX.java
@@ -0,0 +1,26 @@
+package com.manning.androidhacks.hack044;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivityX extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Class<?> activity = null;
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+			activity = MainActivity2X.class;
+		} else {
+			activity = MainActivity3X.class;
+		}
+
+		startActivity(new Intent(this, activity));
+		finish();
+
+	}
+
+}
